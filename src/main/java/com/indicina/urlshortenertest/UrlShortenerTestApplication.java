fb5a2e4e69bce821f8cc1dab0b430da6c6ab3627
blessing.onyia <onyia.blessing2014@gmail.com>
2022-03-06 22:28:28 +0100

    Initial commit

---
+++ src/main/java/com/indicina/urlshortenertest/UrlShortenerTestApplication.java
@@ -0,0 +1,13 @@
+package com.indicina.urlshortenertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indicina/urlshortenertest/UrlShortenerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indicina.urlshortenertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
